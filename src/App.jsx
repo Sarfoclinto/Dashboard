e9d0aa86bba693ea7ad638b971ce239f9dd97cc9
sherlick <wolffighter00000@gmail.com>
2024-07-19 20:01:17 +0000

    Dashboard renamed to Tasklist

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Dashboard from "./Pages/Dashboard";
+import Dashboard from "./Pages/Tasklist";
 function App() {
   return (
     <div className="app w-full h-screen">
       <Router>
         <Routes>
-          <Route path="/" element={<Dashboard />} />
+          <Route path="/" element={<Tasklist />} />
         </Routes>
       </Router>
     </div>
